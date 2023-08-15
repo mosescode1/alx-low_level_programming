73d77db11ffd45928f78fc5a056c69ae3d6f437c
mosescode1 <emosescode@gmail.com>
2023-08-15 11:52:22 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - making lowercase alphabet
+ *
+ * desceription: printing loewercase alphabets
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
