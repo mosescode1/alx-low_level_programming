bb0da9fda70b663aa6e6f3670802a6397db6da18
mosescode1 <emosescode@gmail.com>
2023-08-16 13:51:33 -0700

    100-times_table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints all natural number starting from 0
+ *
+ * @i: first loop value 
+ * @j: second loop value 
+ * @n:  inputed value to be checked
+ */
+void print_times_table(int n)
+{
+	int i; 
+	int j;
+	int k;
+
+	if (n >= 0 &&  n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n ; j++)
+			{
+				k = i * j;
+				if (j == 0)
+				{
+					_putchar('0' + k);
+				}
+				else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
