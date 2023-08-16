0979d47c03304e561ebe13be1272660703f2c311
mosescode1 <emosescode@gmail.com>
2023-08-15 20:09:00 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - getting the last digit of a number
+ * @number: number to be checked
+ *
+ * Return: lastnumber
+ */
+int print_last_digit(int number)
+{
+	int lastnumber;
+
+	if (number > 0)
+	{
+		lastnumber = number % 10;
+	}
+	else
+	{
+		lastnumber = -number % 10;
+	}
+		_putchar('0' + lastnumber);
+		return (lastnumber);
+}
