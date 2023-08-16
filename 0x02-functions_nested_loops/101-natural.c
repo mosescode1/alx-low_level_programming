fb5ab6ff5fbf71fac87d9b28de9dd4415196e6d9
mosescode1 <emosescode@gmail.com>
2023-08-16 14:59:13 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long long int sum;
+	int i;
+
+	for (i == 0; i <= 1024; i++)
+	{
+		if (i = 3 || i = 5)
+		{
+			sum += i;
+		}
+	}
+	return (0);
+}	
