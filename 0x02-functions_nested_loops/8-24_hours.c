1b6ecd61c32899b865ffc513795874c1e0a98d69
mosescode1 <emosescode@gmail.com>
2023-08-16 02:23:42 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Diplaying movie time
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; i <= 59; j++)
+		{
+			_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+			_putchar(':');
+			_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			_putchar('\n');
+		}
+
+	}
+}
