7a8cda5226eb3c2c6459d674d4947fe691457a16
mosescode1 <emosescode@gmail.com>
2023-08-18 05:15:56 -0700

    100-prime_factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	unsigned long int n  = 612852475143;
+	int nf = 2;
+	
+	while (n > 1)
+	{
+		if (n % nf == 0)
+		{
+			n/= nf;
+		}
+		else
+		{
+			nf++;
+		}
+		
+	}
+	printf("%u\n",nf);
+
+	return (0);
+
+}
