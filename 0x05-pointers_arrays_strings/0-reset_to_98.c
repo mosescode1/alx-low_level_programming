44c6f25c160e4c99e2acd74e7e5e411f56329e0e
mosescode1 <emosescode@gmail.com>
2023-08-21 11:05:25 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - as parameter and updates the value it points to to 98
+* @n: parameter to be checked
+* Return: Always 0 (Success)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+##0x05-Pointer Arrays And Strings
