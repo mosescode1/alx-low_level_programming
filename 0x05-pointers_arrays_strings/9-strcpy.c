388a9591b3585d31d443857525d6a5a5d161a2f9
mosescode1 <emosescode@gmail.com>
2023-08-21 19:27:36 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,19 +10,19 @@
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
-	
+
 	len = 0;
-	
+
 	while (src[len] != '\0')
 	{
 		len++;
 	}
-	
+
 	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
-	
+
 	return (dest);
 }
