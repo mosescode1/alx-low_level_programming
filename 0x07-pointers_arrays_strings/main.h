35cd859aca9e7b1f217779498cd99b23e45ca0d8
mosescode1 <emosescode@gmail.com>
2023-08-28 17:31:48 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 int _putchar(char c);
+char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
