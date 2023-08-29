9e17e53473211b53677181f8a0235dfe64737802
mosescode1 <emosescode@gmail.com>
2023-08-28 18:22:03 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
 int _putchar(char c);
+void print_chessboard(char (*a)[8]);
+char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
