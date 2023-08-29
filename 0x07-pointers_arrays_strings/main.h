812dfefada1fc0a60302ce2608a9f0618a807469
mosescode1 <emosescode@gmail.com>
2023-08-28 18:48:23 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 int _putchar(char c);
+void set_string(char **s, char *to);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
