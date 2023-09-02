db7253deacb1c9ffcd3307ecb4bbaa5eeb167c38
mosescode1 <emosescode@gmail.com>
2023-09-01 18:17:12 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+/**
+* main - add all argument  it receives
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
 
-int main(int argc, char *argv[]) {
-    int sum = 0;
-    int i;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    if (argc == 1) {
-        printf("0\n");
-        return 0;
-    }
+	for (i = 1; i < argc; i++)
+	{
+		int j;
 
-    for (i = 1; i < argc; i++) {
-        int j;
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 
-        for (j = 0; argv[i][j] != '\0'; j++) {
-            if (!isdigit(argv[i][j])) {
-                printf("Error\n");
-                return 1;
-            }
-        }
-
-        sum += atoi(argv[i]);
-    }
-
-    printf("%d\n", sum);
-
-    return 0;
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
