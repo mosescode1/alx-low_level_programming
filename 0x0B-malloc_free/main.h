ce123347a7dc265e9a6dc2074953de4531469edc
mosescode1 <emosescode@gmail.com>
2023-09-05 16:59:38 -0700

    101-strtow.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 char *create_array(unsigned int size, char c);
+char **strtow(char *str);
 char *argstostr(int ac, char **av);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
