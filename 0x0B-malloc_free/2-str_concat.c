6d9e338462946ac837d3fbc79bde84c8c208b471
mosescode1 <emosescode@gmail.com>
2023-09-04 17:02:22 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,7 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	int len1 = 0, len2 = 0;
 	char *concat;
-	
+
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -21,7 +21,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	
+
 	while (s1[len1] != '\0')
 	{
 		len1++;
@@ -30,8 +30,8 @@ char *str_concat(char *s1, char *s2)
 	{
 		len2++;
 	}
-	concat = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-	
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+
 	for (i = 0; i < len1; i++)
 	{
 		concat[i] = s1[i];
