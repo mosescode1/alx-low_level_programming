6db6f409cd28ab26c9b813cd13ee0aecea63e6bd
mosescode1 <emosescode@gmail.com>
2023-09-07 15:16:16 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,7 +7,7 @@
 */
 void *malloc_checked(unsigned int b)
 {
-	int *ptr;
+	void *ptr;
 
 	ptr = malloc(b);
 
@@ -15,5 +15,6 @@ void *malloc_checked(unsigned int b)
 	{
 		exit(98);
 	}
+	
 	return (ptr);
 }
