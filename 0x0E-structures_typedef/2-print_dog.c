d4c8afb035b92759ba19c145e54fc10672fe2061
mosescode1 <emosescode@gmail.com>
2023-09-11 18:08:15 -0700

    do

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,10 @@
 #include "dog.h"
 #include <stdio.h>
+/**
+ * print_dog - Entry point
+ * @d: pointer
+ * Description: short
+ */
 
 void print_dog(struct dog *d)
 {
@@ -7,6 +12,6 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner !=NULL ? d->owner : "(nil)");
+		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 	}
 }
