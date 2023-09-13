3436d0f7e5172445bf5bc2e7710500cbff4d3960
mosescode1 <emosescode@gmail.com>
2023-09-13 09:32:55 -0700

    array_iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iteration - executa a function of ecach element of array
+ * @array: array pointer
+ * @size: size of array
+ * @action: call back function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array != NULL && (*action) != NULL)
+	{
+		for(i = 0; i < size; i++)
+		{
+			(*action)(array[i]);
+		}
+	}
+}
