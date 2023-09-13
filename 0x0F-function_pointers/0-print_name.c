3d1d7d4559c9ba0b1c972b48eb1c3e299df97a29
mosescode1 <emosescode@gmail.com>
2023-09-13 09:12:31 -0700

    print-name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints name
+ * @name: string name
+ * @f: pointer function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL}
+		(*f)(name);
+}
