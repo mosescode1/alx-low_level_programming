ce7fc710d31a7a288d065d062d6c32a2ec1de173
mosescode1 <emosescode@gmail.com>
2023-09-21 15:55:22 +0100

    task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * add_node_end - adding node at the end of the list
+ * @head: double pointer
+ * @str: copied string
+ * Return: address
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp, *ptr;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+
+	temp->str = strdup(str);
+
+	if (temp->str == NULL)
+	{
+		return (NULL);
+		free(temp);
+	}
+
+	temp->len = strlen(str);
+	temp->next = NULL;
+	ptr = *head;
+	if (ptr == NULL)
+		*head = temp;
+	else
+	{
+		while (ptr->next != NULL)
+		{
+			ptr = ptr->next;
+		}
+		ptr->next = temp;
+	}
+	return (*head);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,6 +16,7 @@ unsigned int len;
 struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
+list_t *add_node_end(list_t **head, const char *str);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 #endif
