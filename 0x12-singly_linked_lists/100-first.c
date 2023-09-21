ec808c2a9ee9884e366a3d279bba5614c9ab5494
mosescode1 <emosescode@gmail.com>
2023-09-21 16:16:23 +0100

    my constructor

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,9 +2,8 @@
 /**
  * my_constructor - execute before main
  */
-
 void my_constructor(void) __attribute__((constructor));
-void my_constructor(void) 
+void my_constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
