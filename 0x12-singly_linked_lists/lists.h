8f80432fc4bc1334610693d7893bb64f46e33c3a
mosescode1 <emosescode@gmail.com>
2023-09-21 16:32:47 +0100

    all

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,6 +15,7 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
+void free_list(list_t *head);
 size_t print_list(const list_t *h);
 list_t *add_node_end(list_t **head, const char *str);
 size_t list_len(const list_t *h);
