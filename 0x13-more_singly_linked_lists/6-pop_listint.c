ffb965f6eaf59c00da1e7b59a381f5c642fa2d53
mosescode1 <emosescode@gmail.com>
2023-09-26 16:28:17 +0100

    aal

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * pop_listint - pop off the previous head
+ * @head: head pointer
+ * Return: data at the head
+ */
 
 int pop_listint(listint_t **head)
 {
 	int n;
 	listint_t *temp, *ptr;
 
+	if (*head == NULL)
+		return (0);
 	temp = *head;
 	ptr = *head;
 	n = temp->n;
-	if (temp == NULL)
-		return (0);
 
 	temp = temp->next;
 	*head = temp;
