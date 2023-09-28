23d0f602a2e250f4b5543dda7bb6808b6af2cd2f
mosescode1 <emosescode@gmail.com>
2023-09-28 22:04:42 +0100

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * binary_to_uint - converting binary to unsigned int
  * @b: pointer to the string of binary
@@ -6,17 +7,26 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int z;
-	unsigned int dec = 0;
+	int dec = 0;
+	int len = strlen(b), i;
+	int base = 1;
 
-	if (!b)
-		return (0);
+	if (b == NULL)
+		return ((size_t)(NULL));
 
-	for (z = 0; b[z]; z++)
+	for (i = len - 1; i >= 0; i--)
 	{
-		if (b[z] < '0' || b[z] > '1')
+		if (b[i] == '0' || b[i] == '1')
+		{
+			if (b[i] == '1')
+				dec += base; /** dec = 1 + 4**/
+			base *= 2; /** 8**/ 
+
+		}
+		else
+		{
 			return (0);
-		deec = 2 * dec + (b[z] - '0');
+		}
 	}
 	return (dec);
 }
