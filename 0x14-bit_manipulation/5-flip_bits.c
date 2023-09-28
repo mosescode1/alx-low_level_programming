2c384688396ded26803c89e36d2b40d36961f508
mosescode1 <emosescode@gmail.com>
2023-09-28 23:12:16 +0100

    all

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int b, next = 0;
 	unsigned long int nit, pre = n ^ m;
 
-	for (b = 63; b >= 0; a--)
+	for (b = 63; b >= 0; b--)
 	{
 		nit = pre >> b;
 		if (nit & 1)
