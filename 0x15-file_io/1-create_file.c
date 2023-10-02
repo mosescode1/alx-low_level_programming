22efc18bfbbb0093deea00699d9da1b19eec24dc
mosescode1 <emosescode@gmail.com>
2023-10-02 23:30:40 +0100

    fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,11 +21,11 @@ int create_file(const char *filename, char *text_content)
 	{
 		if (fputs(text_content, filepointer) == EOF)
 		{
-			fclose(file_pointer);
+			fclose(filepointer);
 			return (-1);
 		}
 	}
-	fclose(file_pointer);
+	fclose(filepointer);
 	if (chmod(filename, S_IRUSR | S_IWUSR) != 0)
 		return (-1);
 
