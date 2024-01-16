f6d07b27bc206c5737d1eedbdb668f3abb107e2f
mosescode1 <emosescode@gmail.com>
2024-01-16 14:56:06 +0100

    adv

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
-export LD_LIBARY_PATH=.:$LD_LIBARY_PATH
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
